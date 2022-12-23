e2a7e49ab4091611e3fc08c369ddf273623c5d55
StormRooijers <119948244+StormRooijers@users.noreply.github.com>
2022-12-23 08:11:18 +0100

    Update XGBoost.R

--- Predicting/XGBoost.R
+++ Predicting/XGBoost.R
@@ -1,7 +1,6 @@
 install.packages("xgboost")
 library(xgboost)
 
-
 #dependent variable
 train.y <- training$BTC_pricech_t1
 validation.y <- validation$BTC_pricech_t1
@@ -12,9 +11,8 @@ validation.x <- as.matrix(validation[,c(1,3:ncol(validation))])
 
 #Parameters
 parameters <- list(eta = 0.1,
-                   eval_metric = "mae"
-                   )
-
+                   eval_metric = "mae")
+                  
 # Train the model
 model <- xgboost(data = train.x,
                  label = train.y,
@@ -27,21 +25,13 @@ model <- xgboost(data = train.x,
 #Predicting on validation set
 predictions <- predict(model, newdata = validation.x)
 
-# Calculate MAE
-mae <- mean(abs(validation.y - predictions))
-
 #Evaluating model
 mean(validation.y)
 
-# Calculate MSE
+mae <- mean(abs(validation.y - predictions))
 mse <- mean((validation.y - predictions)^2)
-
-# Calculate RMSE
 rmse <- sqrt(mean((validation.y - predictions)^2))
-
-#R-squared
 r2 <- 1 - sum((validation.y - predictions)^2) / sum((validation.y - mean(validation.y))^2)
-
 
 #Plot of the predicted and true values
 plot(validation.y, predictions, xlab = "True values", ylab = "Predicted values")
