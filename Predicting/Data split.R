fde8c31ebe81f4d4fd740da465fcee961c912499
StormRooijers <119948244+StormRooijers@users.noreply.github.com>
2022-12-23 08:05:47 +0100

    Add files via upload

---
+++ Predicting/Data split.R
@@ -0,0 +1,13 @@
+
+
+
+#Determining sizes train,validation and test sets
+training_size <- floor(0.7*nrow(Percentages_2017_2022))
+validation_size <- floor(0.15*nrow(Percentages_2017_2022))
+test_size <- nrow(Percentages_2017_2022) - training_size - validation_size
+
+#Create the different sets
+training <- Percentages_2017_2022[1:training_size,]
+validation <- Percentages_2017_2022[(training_size+1):(training_size+validation_size),]
+test <- Percentages_2017_2022[(training_size+validation_size+1):(training_size+validation_size+test_size),]
+
